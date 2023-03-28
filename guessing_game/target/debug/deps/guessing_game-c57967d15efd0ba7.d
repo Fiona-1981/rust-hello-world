ebf317e8a7a1da7863dc500112f8d6745be82077
Fiona Wiggins <fiona@fionawiggins.co.uk>
2023-03-28 18:12:39 +0100

    Setting up guessing game

---
+++ guessing_game/target/debug/deps/guessing_game-c57967d15efd0ba7.d
@@ -0,0 +1,5 @@
+/Users/fionawiggins/Documents/Dev/rust-hello-world/guessing_game/target/debug/deps/guessing_game-c57967d15efd0ba7: src/main.rs
+
+/Users/fionawiggins/Documents/Dev/rust-hello-world/guessing_game/target/debug/deps/guessing_game-c57967d15efd0ba7.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/fionawiggins/Documents/Dev/rust-hello-world/guessing_game/target/debug/guessing_game: /Users/fionawiggins/Documents/Dev/rust-hello-world/guessing_game/src/main.rs
